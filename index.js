e350dca36ad8fd9fa87394ec5277a024790c5a1d
David Barratt <davidbarratt@iheartmedia.com>
2017-02-05 18:56:37 -0500

    Return data from the database.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello World!');
+const Hapi = require('hapi');
+const mysql = require('mysql');
+
+const server = new Hapi.Server();
+server.connection({ port: 9000, host: 'localhost' });
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  database : 'rest'
+});
+
+connection.connect();
+
+server.route({
+    method: 'GET',
+    path: '/{id}',
+    handler: function (request, reply) {
+      console.log('ID', request.params.id);
+      connection.query('SELECT * FROM users WHERE id = ' +  request.params.id, function (error, results, fields) {
+        if (error) {
+          throw error;
+        }
+        console.log('RESULTS', results);
+        return reply(results[0]);
+      });
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
